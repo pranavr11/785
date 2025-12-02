03907fc1341a4a3b94f694d4c3df13f0d63c4d60
pranavr11 <73618764+pranavr11@users.noreply.github.com>
2025-12-02 10:14:51 -0500

    SAEL

--- hw4lib/model/encoder_layers.py
+++ hw4lib/model/encoder_layers.py
@@ -46,9 +46,17 @@ class SelfAttentionEncoderLayer(nn.Module):
         # TODO: Implement __init__
 
         # TODO: Initialize the sublayers      
-        self.self_attn = NotImplementedError # Self-attention layer
-        self.ffn = NotImplementedError # Feed-forward network
-        raise NotImplementedError # Remove once implemented
+        self.self_attn = SelfAttentionLayer(
+            d_model=d_model,
+            num_heads=num_heads,
+            dropout=dropout,
+            causal=False  
+        ) 
+        self.ffn = FeedForwardLayer(
+            d_model=d_model,
+            d_ff=d_ff,
+            dropout=dropout
+        ) 
 
     def forward(self, x: torch.Tensor, key_padding_mask: Optional[torch.Tensor] = None) -> Tuple[torch.Tensor, torch.Tensor]:
         '''
@@ -64,8 +72,12 @@ class SelfAttentionEncoderLayer(nn.Module):
         # TODO: Implement forward: Follow the figure in the writeup
 
         # What will be different from decoder self-attention layer?
-        x, mha_attn_weights = NotImplementedError, NotImplementedError
+        x, mha_attn_weights = self.self_attn(
+            x, x, x,
+            key_padding_mask=key_padding_mask
+        )
+        x = self.ffn(x)
         
         # TODO: Return the output tensor and attention weights
-        raise NotImplementedError # Remove once implemented
+        return x, mha_attn_weights # Remove once implemented
 
